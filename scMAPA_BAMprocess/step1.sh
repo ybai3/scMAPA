5675a6737c72944e05db14e242bf244509dcc940
ybai3 <43503942+ybai3@users.noreply.github.com>
2020-07-15 14:06:43 -0400

    Ver 0.1.0

--- scMAPA_BAMprocess/step1.sh
+++ scMAPA_BAMprocess/step1.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-module load gcc/8.2.0
-#module load python/bioconda-3.7-2019.03
-#conda acticate fanenv
-
-#./step1.sh path_to_cluster_file path_to_bam_file
+# Python script called in step1.sh file was coded by Ming Tang, shared on https://hackmd.io/X2K8TpKeRJyb2ibK8PLzew?view
+# Zhenjiang Fan modified it to a shell script.
+# To run step1.sh, please first load Python with dependent modules.
+# ./step1.sh path_to_cluster_file path_to_bam_file
 
 python split_bam.py $1 $2
 
--- scMAPA_BAMprocess/step2.sh
+++ scMAPA_BAMprocess/step2.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-module load gcc/8.2.0
-module load samtools/1.9
-module load bedtools/2.29.0
-
+# Step2.sh is a shell script that converts splitted BAM to BEDGRAPh.
+# Zhenjiang Fan modified it to a shell script.
+# Please load samtools and bedtools first.
 # ./step2.sh path_to_bam_directory path_to_output_bedgraph_directory
 
 bam_files=$1/*.bam
@@ -16,8 +15,8 @@ for file in $bam_files; do
 	bam_name=$(echo "$bam_file_name" | cut -f 1 -d '.')
 	
     SORT=$bam_name.sorted.bam
-    BEDGRAPH=$bam_name.bedgraph
-    NEWBEDGRAPH=$2/$bam_name.bedgraph
+    BEDGRAPH=$bam_name.temp.bedgraph
+    NEWBEDGRAPH=$bam_name.bedgraph
 	
 	echo $BAM
 	echo $bam_name
@@ -26,9 +25,10 @@ for file in $bam_files; do
 	echo $NEWBEDGRAPH
 	
 	samtools sort $BAM -o $SORT
-    bedtools genomecov -ibam $SORT -bg > $BEDGRAPH
-    awk '{print "chr" $0}' $BEDGRAPH > $NEWBEDGRAPH
-    rm $BEDGRAPH
+        bedtools genomecov -ibam $SORT -bg > $BEDGRAPH
+        awk '{print "chr" $0}' $BEDGRAPH > $NEWBEDGRAPH
+        rm $BEDGRAPH
+	rm $SORT
 done
 
 
