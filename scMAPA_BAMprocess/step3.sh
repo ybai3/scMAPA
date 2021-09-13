503f665172029253a78987b5fc74ad1eb0ba70cd
ybai3 <43503942+ybai3@users.noreply.github.com>
2021-09-13 10:36:10 -0400

    revise comment line, no impact on function

--- scMAPA_BAMprocess/step3.sh
+++ scMAPA_BAMprocess/step3.sh
@@ -2,7 +2,6 @@
 
 #step3.sh will call modified DaPars2 to estimate the proximal PA site and the abundance of long/short isoforms for each cluster.
 #The location of input files should be specified in the configuration.txt. The desired chromosome ID should be indicated in chrID.txt.
-#Please load the following modules before running step3.sh
 #./step3.sh configuration.txt chrID.txt
 
 cfg_files=$1
